d5454349d3c21f56dba6776b934d6e823beb49e9
SATYA SAI DEVI ARUMILLI <23MH1A4905@acoe.edu.in>
2025-12-14 13:13:38 +0530

    Time: 3 ms (79.70%) | Memory: 43.4 MB (26.07%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        //int[] stack=new int[s.length()];
+        Stack<Character> stack=new Stack<>();
+       for(char ch:s.toCharArray()){
+        if(ch=='['||ch=='{'||ch=='('){
+            stack.push(ch);
+        }
+        else{
+            if(stack.isEmpty()){
+                return false;
+            }
+            char top=stack.pop();
+            if(ch==')'&&top!='('){
+                return false;
+            }
+            if(ch=='}'&&top!='{'){
+                return false;
+            }
+            if(ch==']'&& top!='['){
+                return false;
+            }
+            }
+        }
+       return stack.isEmpty();
+    }
+}
