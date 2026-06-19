4605b0321846abee07fb7977d0c06bf66f277423
ARUMILLI SATYA SAI DEVI <23MH1A4905@acoe.edu.in>
2026-06-19 12:33:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (18.48%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int n=gain.length;
+        int[] high=new int[n+1];
+        high[0]=0;
+        for(int i=0;i<gain.length;i++){
+            high[i+1]=gain[i]+high[i];
+        }
+        int max=high[0];
+        for(int i=1;i<n+1;i++){
+            if(high[i]>max){
+                max=high[i];
+            }
+        }
+        return max;
+    }
+}
