6d4d0d3c9f573201e62abee62683e611f1717343
ARUMILLI SATYA SAI DEVI <23MH1A4905@acoe.edu.in>
2026-06-25 22:51:52 +0530

    Time: 5 ms (15.52%) | Memory: 43 MB (10.97%) - LeetSync

--- 504-base-7/base-7.java
+++ 504-base-7/base-7.java
@@ -1,16 +1,16 @@
 class Solution {
     public String convertToBase7(int num) {
-        if(num == 0) return "0";
-        boolean negative = num < 0;
-        num = Math.abs(num);
         String base="";
-        while(num!=0){
-            
-            base+=num%7;
-            num=num/7;
+        boolean neg=num<0;
+        int abs=Math.abs(num);
+        if(num==0){ 
+            return "0";
         }
-        String ans = new StringBuilder(base).reverse().toString();
-
-        return negative ? "-" + ans : ans;
+        while(abs>0){
+            base+=abs%7;
+            abs=abs/7;
+        }
+        String ans=new StringBuilder(base).reverse().toString();
+        return neg ? "-"+ans : ans;
     }
 }
